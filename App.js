d41e235727ae795ba82cfafeb6632e4be79cd226
Jean Lucca Zampieri <lucca.zampieri@gmail.com>
2024-09-02 21:42:30 -0300

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/firebaseConfig.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CadastroUsuario from "./src/pages/cadastro/Index";
+const Stack = createStackNavigator();
 
 export default function App() {
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CadastroUsuario">
+        <Stack.Screen
+          name="CadastroUsuario"
+          options={{ headerShown: false, headerLeft: null }}
+          component={CadastroUsuario}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
